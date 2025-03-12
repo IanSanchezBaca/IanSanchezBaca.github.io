38801fc2017b40f6363ec1b3b04594d9ce9249bd
IanSanchezBaca <iansanchez066@gmail.com>
2025-03-11 21:17:55 -0700

    changed back

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const resume = document.getElementById("resume-container");
 
-    function getDPI() {
-        return window.devicePixelRatio * 96; // Most screens assume 96 DPI by default
-    }
-
-    function inchesToPixels(inches) {
-        return inches * getDPI();
-    }
-
     function centerResume() {
         let screenWidth = window.innerWidth;
         let screenHeight = window.innerHeight;
 
-        let marginInInches = 4; // 4 inches from each side
-        let width = screenWidth - 2 * inchesToPixels(marginInInches);
-        let height = screenHeight - 2 * inchesToPixels(marginInInches);
+        let width = screenWidth * (4 / 16);
+        let height = screenHeight * (4 / 16);
+        // 4/16 should now be 25% of all edges
 
         resume.style.width = width + "px";
         resume.style.height = height + "px";
+
         resume.style.left = (screenWidth - width) / 2 + "px";
         resume.style.top = (screenHeight - height) / 2 + "px";
     }
