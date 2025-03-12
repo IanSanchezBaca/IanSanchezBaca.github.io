38f9875ab483b62e022d3be3ed0d86728e33bdcc
IanSanchezBaca <iansanchez066@gmail.com>
2025-03-11 21:15:42 -0700

    changed it so that its 4/16ths "inches" away from each other

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     const resume = document.getElementById("resume-container");
 
+    function getDPI() {
+        return window.devicePixelRatio * 96; // Most screens assume 96 DPI by default
+    }
+
+    function inchesToPixels(inches) {
+        return inches * getDPI();
+    }
+
     function centerResume() {
         let screenWidth = window.innerWidth;
         let screenHeight = window.innerHeight;
 
-        let width = screenWidth * (8 / 16); // 50% of width (4/16 from each side)
-        let height = screenHeight * (8 / 16); // 50% of height (4/16 from each side)
+        let marginInInches = 4; // 4 inches from each side
+        let width = screenWidth - 2 * inchesToPixels(marginInInches);
+        let height = screenHeight - 2 * inchesToPixels(marginInInches);
 
         resume.style.width = width + "px";
         resume.style.height = height + "px";
-
         resume.style.left = (screenWidth - width) / 2 + "px";
         resume.style.top = (screenHeight - height) / 2 + "px";
     }
